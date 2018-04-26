1aff604434054d9d2cf7f842f2adb4824896133a
Sarah Zhou <sarahsz@jbglaw.cs.washington.edu>
2018-04-26 12:02:59 -0700

    new tests

--- refactor/test2.py
+++ refactor/test2.py
@@ -1,31 +1,25 @@
 import os
 import commands
 script_dir = os.getcwd()
-os.system('rm -rf tests')
 os.system('mkdir tests && cd tests && mkdir src')
 os.chdir('tests/src')
 os.system('git clone https://github.com/Dilraj-Singh-Devgun/Data-Structures.git')
-os.chdir('Data-Structures/src/Stack')
+os.chdir('Data-Structures/src/Queue')
 
-f = open('ArrayBasedStack.java', 'r')
+f = open('LinkedListBasedQueue.java', 'r')
 contents = f.readlines()
 f.close()
 
-contents.insert(0,'public void foo(){} \n')
-f = open('ArrayBasedStack.java', 'w')
+contents.insert(0, '// This new comment should be a refactor!\n')
+f = open('LinkedListBasedQueue.java', 'w')
 contents = "".join(contents)
 f.write(contents)
 f.close()
 
 os.chdir(script_dir + '/tests/src')
 
-os.system('echo "tests/src/Data-Structures/src/Stack/ArrayBasedStack.java" > files.txt')
+os.system('echo "tests/src/Data-Structures/src/Stack/LinkedListBasedQueue.java" > files.txt')
 os.chdir(script_dir)
 s = 'python refactor.py tests/src/files.txt tests/src/Data-Structures'
 os.system(s)
 
-# result = commands.getoutput('wc -c output_file.txt')
-
-# print result
-
-#Data-Structures/src/DataStructures.java
