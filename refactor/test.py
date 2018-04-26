af9b41f7ad449963cfa5f42d731a9575141ca4ce
Evan Blajev <eblajev@general.cs.washington.edu>
2018-04-25 22:44:26 -0700

    test for refactoring

--- refactor/test.py
+++ refactor/test.py
@@ -1,6 +1,7 @@
 import os
 import commands
 script_dir = os.getcwd()
+os.system('rm -rf tests')
 os.system('mkdir tests && cd tests && mkdir src')
 os.chdir('tests/src')
 os.system('git clone https://github.com/Dilraj-Singh-Devgun/Data-Structures.git')
@@ -10,7 +11,7 @@ f = open('ArrayBasedStack.java', 'r')
 contents = f.readlines()
 f.close()
 
-contents.insert(0,'// TEST \n')
+contents.insert(0,'public void foo(){} \n')
 f = open('ArrayBasedStack.java', 'w')
 contents = "".join(contents)
 f.write(contents)
@@ -18,12 +19,13 @@ f.close()
 
 os.chdir(script_dir + '/tests/src')
 
-os.system('echo "/tests/src/Data-Structures/src/Stack/ArrayBasedStack.java" > files.txt')
+os.system('echo "tests/src/Data-Structures/src/Stack/ArrayBasedStack.java" > files.txt')
 os.chdir(script_dir)
-s = 'python refactor.py tests/src/files.txt'.format(script_dir)
+s = 'python refactor.py tests/src/files.txt tests/src/Data-Structures'
 os.system(s)
 
-result = commands.getoutput('wc -c output_file.txt')
+# result = commands.getoutput('wc -c output_file.txt')
 
-print result
+# print result
 
+#Data-Structures/src/DataStructures.java
