6c0f44d6553e59e9a43f4180150d72be063549a9
connerk <connerk@uw.edu>
2018-04-26 16:36:02 -0700

    added to test

--- tests/run-deltaTests.py
+++ tests/run-deltaTests.py
@@ -1,15 +1,18 @@
 #!/user/bin/python
+import sys
+sys.path.append('../run-delta.py')
+from run-delta.py import parse_diff()
 
 def main():
     testParseDiff()
 
 def testParseDiff():
-    diffFile = open("../tmp/fullDiff.txt")
-    readDiff = open("testDiff.txt")
+    diffFile = open("../tmp/fullDiff.txt","w+")
+    readDiff = open("testDiff.txt","w+")
     ls = readDiff.read().splitlines()
     for l in ls:
         diffFile.write(l)
-    actual = parseDiff()
+    actual = parse_diff()
     print actual
 
 
