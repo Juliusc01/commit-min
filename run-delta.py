dae19ee221ecdaec8f24a52d1a1808f8b7ed6496
Jacob Ward <jacobmward23@gmail.com>
2018-04-25 17:50:20 -0700

    Fix spacing issues

--- run-delta.py
+++ run-delta.py
@@ -1,36 +1,40 @@
+#!/usr/bin/python
 from subprocess import call
 
 def main():
-   dict1 = {}
-   myFile = open("files.txt")
-   lines = myFile.readlines();
-   myList = []
-   singleFile = None
-   inChanges = False
-   for line in lines:
-	if(line.startswith("+++")) :
-		inChanges = True
-		singleFile = line[6:]
-	elif((line.startswith("+") or line.startswith("-")) and inChanges) :
-		myList.append(line)
-		
-	if(line.startswith("diff") and inChanges) :
-		dict1[singleFile] = myList
-		myList = []
-		singleFile = None
-		inChanges = False
+  dict1 = {}
+  myFile = open("files.txt")
+  lines = myFile.readlines()
+  myList = []
+  singleFile = None
+  inChanges = False
+  for line in lines:
+    line = line[:-1]
+    if(line.startswith("+++")):
+      inChanges = True
+      singleFile = line[6:]
+    elif((line.startswith("+") or line.startswith("-")) and inChanges):
+      myList.append(line)
 	
-   dict1[singleFile] = myList
-   myFile.close()
+    if(line.startswith("diff") and inChanges):
+      dict1[singleFile] = myList
+      myList = []
+      singleFile = None
+      inChanges = False
+
+    dict1[singleFile] = myList
+    myFile.close()
    
 
-   # takes a set, and concats into space seperated string
-   print dict1
-   files = ' '.join(str(path) for path in dict1.keys())
-   multi = '~/commit-min/delta/multidelta'
-   #call("", shell=True)
-   print files
+  # takes a set, and concats into space seperated string
+  print dict1
+  files = ' '.join(str(path) for path in dict1.keys())
+  multi = '~/multidelta'
+  test_script = '~/commit-min/delta/mvn.test'
+  run_delta = multi + ' ' + test_script + ' ' + files
+  #call(run_delta, shell=True)
+  print files
 
 if __name__ == '__main__':
-    main()
+  main()
 
