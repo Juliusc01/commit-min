50078b10d8816ea6d402d0f8047259b4158d9531
conner knight <connerk@uw.edu>
2018-04-26 12:22:24 -0700

    added travis and test file

---
+++ tests/run-deltaTests.py
@@ -0,0 +1,16 @@
+#!/user/bin/python
+
+def main():
+
+def testParseDiff():
+    diffFile = open("/tmp/fullDiff.txt")
+    readDiff = open("testDiff.txt")
+    ls = readDiff.read().splitlines()
+    for l in ls:
+        diffFile.write(l)
+    }
+    parseDiff()
+
+
+if __name__ == '__main__':
+    main()
