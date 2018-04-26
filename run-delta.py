0ea27d8e0411b9719b585c2c4bc8ea53c73bb0e1
Jacob Ward <jacobmward23@gmail.com>
2018-04-25 22:42:31 -0700

    Cleaned up and fixed to work with argument and mvn project

--- run-delta.py
+++ run-delta.py
@@ -1,40 +1,58 @@
 #!/usr/bin/python
 from subprocess import call
+import sys
+import signal
+
+def parse_diff():
+  path_to_diffs = {}  # map from path to list of diff lines
+  diffs = []
+  path = None
+  has_changes = False
+
+  diff_file = open("/tmp/fullDiff.txt")
+  lines = diff_file.read().splitlines()
+  root_repo = lines.pop(0)  # path to the root of the repo
+
+  # loop over lines, adding diffs to map
+  for line in lines:
+    if(line.startswith("+++")):
+      has_changes = True
+      path = root_repo + line[5:]
+
+    # + and - and the beginning of a line signifies a change
+    elif((line.startswith("+") or line.startswith("-")) and has_changes):
+      diffs.append(line)
+	
+    # check if this is a new file
+    if(line.startswith("diff") and has_changes):
+      path_to_diffs[path] = diffs
+      diffs = []
+      path = None
+      has_changes = False
+
+  path_to_diffs[path] = diffs
+  diff_file.close()
+
+  return path_to_diffs
 
 def main():
-  dict1 = {}
-  myFile = open("fullDiff.txt")
-  lines = myFile.readlines()
-  myList = []
-  singleFile = None
-  inChanges = False
-  for line in lines:
-    line = line[:-1]
-    if(line.startswith("+++")):
-      inChanges = True
-      singleFile = line[6:]
-    elif((line.startswith("+") or line.startswith("-")) and inChanges):
-      myList.append(line)
-	
-    if(line.startswith("diff") and inChanges):
-      dict1[singleFile] = myList
-      myList = []
-      singleFile = None
-      inChanges = False
-
-    dict1[singleFile] = myList
-    myFile.close()
-   
+  path_to_diffs = parse_diff() 
 
   # takes a set, and concats into space seperated string
-  print dict1
-  files = ' '.join(str(path) for path in dict1.keys())
+  print path_to_diffs
+  files = ' '.join(str(path) for path in path_to_diffs.keys())
   multi = '~/multidelta'
   test_script = '~/commit-min/delta/mvn.test'
-  run_delta = multi + ' ' + test_script + ' ' + files
-  #call(run_delta, shell=True)
+  unit_test = '-unit_test=' + str(sys.argv[1])
+  run_delta = multi + ' ' + unit_test + ' ' + test_script + ' ' + files
+  #call(run_delta)
   print run_delta
 
+def interrupt_handler():
+  # do something to kill multidelta and clean up files
+  sys.exit(0)
+
 if __name__ == '__main__':
+  signal.signal(signal.SIGINT, interrupt_handler)
   main()
 
