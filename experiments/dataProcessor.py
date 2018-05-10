cd30765ed79a4ed9b5bf6ed413475c17b10bc1be
Conner Knight <connerk@cs.washington.edu>
2018-05-09 22:54:42 -0700

    added dataProcessor along with example diffs

---
+++ experiments/dataProcessor.py
@@ -0,0 +1,72 @@
+#!/user/bin/python
+import plotly
+from plotly.graph_objs import Scatter, Layout
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+
+def main():
+
+    allFiles = open("diffFiles")
+
+    fileList = allFiles.read().splitlines()
+
+    if(len(fileList) % 2 != 0):
+        print("diffFiles must be even")
+        return
+    i = 0
+    accsList = []
+    precsList = []
+    comList = []
+    while i < len(fileList) - 2 :
+        ourFile = open(fileList[i])
+        expFile = open(fileList[i+1])
+        rawOurOutput = ourFile.read().splitlines()
+        outSet = set(rawOurOutput)
+
+        rawExpOutput = expFile.read().splitlines()
+        expSet = set(rawExpOutput)
+
+        accSet = expSet - outSet
+        precSet = outSet - expSet
+
+        accuracy = (len(expSet) - len(accSet)) / (len(expSet))
+        precision = (len(outSet) - len(precSet)) / (len(outSet))
+
+        accsList.append(accuracy)
+        precsList.append(precision)
+        comList.append(fileList[i+2])
+        i+= 3
+
+    plotly.offline.plot({
+       "data": [Scatter
+            (x=comList, y=accsList)],
+        "layout": Layout(title="Accuracy Graph",
+                          xaxis=dict(title='commit size'),
+                          yaxis=dict(title='accuracy', range=[0,1.05]))
+    })
+
+    plotly.offline.plot({
+        "data": [Scatter
+                 (x=comList, y=precsList)],
+        "layout": Layout(title="Precision Graph",
+                         xaxis=dict(title='commit size'),
+                         yaxis=dict(title='precision', range=[0, 1.05]))
+    })
+
+    #data = [accTrace]
+    #py.iplot(data, filename='scatter-mode')
+
+
+    print("accuracy", accsList[1])
+    print("precision", precsList[1])
+
+    '''
+    plotly.offline.plot({
+        "data": [Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
+        "layout": Layout(title="hello world")
+    })
+    '''
+
+if __name__ == '__main__':
+    main()
