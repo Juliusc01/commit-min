6047d2ea7b1b827e5011156ddf7c53f8c36b0e02
Conner Knight <connerk@cs.washington.edu>
2018-05-10 17:04:45 -0700

    hopefully fixed exp

--- experiments/dataProcessor.py
+++ experiments/dataProcessor.py
@@ -1,6 +1,6 @@
 #!/user/bin/python
 import plotly
-from plotly.graph_objs import Scatter, Layout
+from plotly.graph_objs import Scatter, Layout, Table
 import plotly.plotly as py
 import plotly.graph_objs as go
 
@@ -53,6 +53,15 @@ def main():
                          xaxis=dict(title='commit size'),
                          yaxis=dict(title='precision', range=[0, 1.05]))
     },filename='precisionGraph.html', auto_open=False)
+
+
+
+    plotly.offline.plot({
+        "data": [Table( header=dict(values=['Commit size', 'percent of bug fix lines found']),
+    cells=dict(values=[comList,
+                       accsList]))],
+        "layout": Layout(title="commit size vs accuracy")
+    })
 
     #data = [accTrace]
     #py.iplot(data, filename='scatter-mode')
